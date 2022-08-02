efdd6c7cb8dfe693f47974e2ed779d2f92ff275f
Brian Ginsburg <gins@brianginsburg.com>
2022-08-01 21:02:19 -0700

    Start on commentary

---
+++ src/commentary.ts
@@ -0,0 +1,16 @@
+type Commentary = [string, number][]
+
+const welcome: Commentary = [
+  ['Oh', 300],
+  ['Hello there', 300]
+]
+
+const utterNonesense: Commentary = [
+
+]
+
+const critiques: Commentary = [
+
+]
+
+exports.welcome = welcome
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
 const fs = require('fs')
+const path = require('path')
 
 const didTransformers = require('ucans/did/transformers')
 const ucans = require('ucans')
 const uint8arrays = require('uint8arrays')
 const { EdKeypair } = require('ucans/keypair/ed25519')
 
+const commentary = require('./commentary')
+
 const main = async () => {
+  const args: string[] = process.argv.slice(2);
+
   const keypair = await loadKeypair()
+
+  if (args.length === 0) {
+    // welcome
+
+    console.log(commentary.welcome)
+
+
+    // emit UCAN for registry
+
+  } else {
+    const route = args[0]
+    const proof = args[1]
+
+    // check proof
+
+    // comment
+
+    // emit UCAN
+  }
 
   // console.log("keypair", keypair)
 }
 
 const loadKeypair = async (): Promise<typeof EdKeypair> => {
-  const keyPath = __dirname + '/SECRET_KEY'
-  const didPath = __dirname + '/DID'
+  const root = path.resolve(__dirname, '..')
+  const keyPath = root + '/SECRET_KEY'
+  const didPath = root + '/DID'
 
   try {
     const secretKey = fs.readFileSync(keyPath).toString()
